883dce973aa3862c5f48b8c3fa49c06332db2d28
0x3b3fc <dev.sief.hesham@gmail.com>
2022-07-21 16:56:37 +0200

    retrieve Data from Database using models

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -11,8 +11,36 @@
     <title>Eloquent - Laravel</title>
 </head>
 <body>
-<h1>Index</h1>
 
+<div class="container">
+    <div class="row">
+        <h1>Index</h1>
+        <a href="{{ route('posts.create') }}" class="btn btn-primary">إضافة بوست</a>
+        <table class="table table-striped table-hover">
+            <thead>
+            <tr>
+                <th scope="col">#ID</th>
+                <th scope="col">Title</th>
+                <th scope="col">Body</th>
+                <th scope="col">Action</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($posts as $post)
+            <tr>
+                <th scope="row">{{ $post->id }}</th>
+                <td>{{ $post->title }}</td>
+                <td>{{ $post->body }}</td>
+                <td>
+                    <a href="#" class="btn btn-warning">Edit</a>
+                    <a href="#" class="btn btn-danger">Delete</a>
+                </td>
+            </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
 
 <!-- Option 1: Bootstrap Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
